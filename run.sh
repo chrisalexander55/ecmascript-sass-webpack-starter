283d57b7e0adc48394e62b7f9b60da1ff97b0dda
chrisalexander55 <chrisalexander55@gmail.com>
2017-08-22 02:22:37 +0000

    updated extract-text-webpack-plugin

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #! bin/bash
-docker build -t ecmascript-webpack-starter-cntr .
-docker run -it --rm -v "$PWD":/usr/src/ecmascript-webpack-starter -p 3000:3000/tcp ecmascript-webpack-starter-cntr /bin/bash
+docker build -t ecmascript-sass-webpack-starter-cntr .
+docker run -it --rm -v $PWD:/usr/src/ecmascript-sass-webpack-starter -p 3000:3000/tcp ecmascript-sass-webpack-starter-cntr /bin/bash
